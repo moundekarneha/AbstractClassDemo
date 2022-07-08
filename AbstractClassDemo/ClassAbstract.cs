648067d35c8c24b5eafe6904967d6bb8bc1357f5
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 17:01:53 +0530

    Add project files.

---
+++ AbstractClassDemo/ClassAbstract.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassDemo
+{
+    internal abstract class ClassAbstract
+    {
+        public void nonAbstractMethod()
+        {
+            Console.WriteLine("Non abstract method");
+        }
+
+        public abstract void abstractMethod();
+    }
+
+    internal class ClassTest: ClassAbstract
+    {
+        public override void abstractMethod()
+        {
+            Console.WriteLine("Abstract method");
+        }
+
+        public static void Main()
+        {
+            ClassTest t = new ClassTest();
+            ClassAbstract a = t;
+            t.abstractMethod();
+            t.nonAbstractMethod();
+            a.abstractMethod();
+            a.nonAbstractMethod();
+            Console.ReadLine();
+        }
+    }
+}
